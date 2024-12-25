c8d558cf540a6084381eec73a6eb0a62737330cc
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2024-12-25 15:02:39 -0500

    fixed up pawn logic

--- chess/src/app/pieces/pawn.ts
+++ chess/src/app/pieces/pawn.ts
@@ -15,48 +15,38 @@ export class Pawn extends Piece {
         const prevRow = this.getPrevRow()
         const prevCol = this.getprevCol()
 
+        const initialPawnMove = prevRow === 6 ? -2 : 2
+        const pawnMove = this.getColor() == ChessColor.BLACK ? -1: +1
+
+        if(prevRow === 6  || prevRow === 1 ){
+            const newMove = [prevRow+initialPawnMove, prevCol]
+            newMoves.push(newMove)
+        }
+
+        if(chessboard[prevRow+pawnMove][prevCol]==null){
+            const newMove = [prevRow+pawnMove, prevCol]
+            newMoves.push(newMove)
+        }
+
+        if(chessboard[prevRow+pawnMove][prevCol+pawnMove]!=null && chessboard[prevRow+pawnMove][prevCol+pawnMove]?.getColor()!=this.getColor()){
+            const newMove = [prevRow-1, prevCol-1]
+            newMoves.push(newMove)
+        }
+
+
         if(this.getColor() === ChessColor.BLACK){
-            if(prevRow === 6 ) {
-                const newMove = [prevRow-2, prevCol]
-                newMoves.push(newMove)
-            }
 
-            if(chessboard[prevRow-1][prevCol-1]!=null && chessboard[prevRow-1][prevCol-1]?.getColor()!=this.getColor()){
-                const newMove = [prevRow-1, prevCol-1]
-                newMoves.push(newMove)
-            }
-
-            if(chessboard[prevRow-1][prevCol+1]!=null && chessboard[prevRow-1][prevCol+1]?.getColor()!=this.getColor()){
+            if(chessboard[prevRow+pawnMove][prevCol+1]!=null && chessboard[prevRow-1][prevCol+1]?.getColor()!=this.getColor()){
                 const newMove = [prevRow-1, prevCol+1]
                 newMoves.push(newMove)
-            }
-
-            if(chessboard[prevRow-1][prevCol]?.getColor()!=ChessColor.WHITE){
-                const newMove = [prevRow-1, prevCol]
-                newMoves.push(newMove)
-            }
-          
+            }      
 
         }
 
         else{
-            if(prevRow === 1 ) {
-                const newMove = [prevRow+2, prevCol]
-                newMoves.push(newMove)
-            }
 
-            if(chessboard[prevRow+1][prevCol-1]!=null && chessboard[prevRow+1][prevCol-1]?.getColor()!=this.getColor()){
+            if(chessboard[prevRow+pawnMove][prevCol-1]!=null && chessboard[prevRow+1][prevCol-1]?.getColor()!=this.getColor()){
                 const newMove = [prevRow+1, prevCol-1]
-                newMoves.push(newMove)
-            }
-
-            if(chessboard[prevRow+1][prevCol+1]!=null && chessboard[prevRow+1][prevCol+1]?.getColor()!=this.getColor()){
-                const newMove = [prevRow+1, prevCol+1]
-                newMoves.push(newMove)
-            }
-
-            if(chessboard[prevRow+1][prevCol]?.getColor()!=ChessColor.BLACK){
-                const newMove = [prevRow+1, prevCol]
                 newMoves.push(newMove)
             }
           
